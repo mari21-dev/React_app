ec841879a92352e7acd9359f815facbe90efca3f
Marina Dubinina <dubinina.m.21@gmail.com>
2021-10-15 17:23:45 +0300

    Install "react-router-dom". Add page "Dialogs" and link (route,browser-router)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import s from './Navbar.module.css'
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}><a>Profile</a></div>
-            <div className={s.item}><a>Messages</a></div>
+            <div className={s.item}><a href="/profile">Profile</a></div>
+            <div className={s.item}><a href="/dialogs">Messages</a></div>
             <div className={s.item}><a>News</a></div>
             <div className={s.item}><a>Music</a></div>
             <div className={s.item}><a>Settings</a></div>
