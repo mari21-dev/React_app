4297496bf54f3f95ed684de3df6684e4299a0a8c
Marina Dubinina <dubinina.m.21@gmail.com>
2021-10-15 16:08:07 +0300

    Refactoring and add Props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,11 +11,8 @@ const MyPosts = () => {
             </div>
             <div>New post</div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message='Hi, how are you?'/>
+                <Post message="It's my first post"/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img
-                src="https://kartinkin.net/uploads/posts/2021-07/1625174407_5-kartinkin-com-p-blondinka-s-golubimi-glazami-art-art-krasi-6.jpg"
+                src="https://lh3.googleusercontent.com/proxy/4aj4j0Bo8XjFBJJz3dCzedJCEzwEwWkgsmVpzsFvWOPg--w94-Yz3pxJHolvRc1y3zWq3ChfJlM5mguSsPNvDmRGvffdBwA23Ibzo3shuigsPHn6rJDZrekcqFZmpQ1XlolQUdaBX835cTXZjl-atVY"
                 alt=""/>
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
