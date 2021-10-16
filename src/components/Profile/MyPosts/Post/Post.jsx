8926d0684f5841b4d35b1cf8ac7a32eacfd6bfb5
Marina Dubinina <dubinina.m.21@gmail.com>
2021-10-16 16:43:06 +0300

    Layout of the dialog page

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,7 @@ const Post = (props) => {
     return (
         <div className={s.item}>
             <img
-                src="https://lh3.googleusercontent.com/proxy/4aj4j0Bo8XjFBJJz3dCzedJCEzwEwWkgsmVpzsFvWOPg--w94-Yz3pxJHolvRc1y3zWq3ChfJlM5mguSsPNvDmRGvffdBwA23Ibzo3shuigsPHn6rJDZrekcqFZmpQ1XlolQUdaBX835cTXZjl-atVY"
+                src="https://pp.userapi.com/c836320/v836320225/63fbc/-AL8gmuPvCM.jpg"
                 alt=""/>
             {props.message}
             <div>
