efd9ad179ded25fe44324fb7920c63d0ee920803
Marina Dubinina <dubinina.m.21@gmail.com>
2021-10-16 17:00:30 +0300

    First components lesson

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import s from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Dmitriy</div>
+                <div className={s.dialog}>Darya</div>
+                <div className={s.dialog}>Yuliya</div>
+                <div className={s.dialog}>Marina</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
+    )
+}
+
+export default Dialogs;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src="https://templates.iqonic.design/socialv/bs5/html/dist/assets/images/logo.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
