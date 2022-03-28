397e318af290a2f5a550c93fac7564795c0bf423
Marina <dubinina.m.21@gmail.com>
2022-03-28 17:09:14 +0300

    Rest API, Users Api, Side effect, get users

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/usets-reducer";
+import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
     return {
