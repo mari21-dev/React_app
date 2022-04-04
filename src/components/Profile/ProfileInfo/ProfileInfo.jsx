b1f85a36841d860f37bf60c3ac3851440a049cab
Marina <dubinina.m.21@gmail.com>
2022-04-04 20:56:48 +0300

    Working with the Profile page, added ajax, working with api

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+
+    if (!props.profile) {
+        return <Preloader/>
+    }
+
     return (
         <div>
             <div><img
                 src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
                 alt=""/></div>
-            <div className={s.descriptionBlock}>ava+description</div>
+            <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large}/>
+                ava+description
+            </div>
         </div>
     )
 }
