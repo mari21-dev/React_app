9e8ec1584d1b10faca2548c03a3be2f4152d88df
Marina <dubinina.m.21@gmail.com>
2022-04-16 02:29:13 +0300

    Added redux-form for form on ProfilePage, Messages, Login

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,32 +1,20 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {Field, reduxForm} from "redux-form";
 
 const MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => <Post message={p.message} key={p.id} likesCount={p.likesCount}/>);
 
-    let newPostElement = React.createRef();
-
-    let onAddPost = () => {
-        props.addPost()
-    }
-
-    let onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText)
     }
 
     return (
-        <div className={s.postsBlock}><h3>My posts</h3>
-            <div>
-                <div><textarea onChange={onPostChange} cols="15" rows="2" ref={newPostElement}
-                               value={props.newPostText}/></div>
-                <div>
-                    <button onClick={onAddPost}>Add post
-                    </button>
-                </div>
-            </div>
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <AddNewPostFormRedux onSubmit={onAddPost}/>
             <div>New post</div>
             <div className={s.posts}>
                 {postsElements}
@@ -34,5 +22,22 @@ const MyPosts = (props) => {
         </div>
     )
 }
+
+const AddNewPostForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component="textarea" cols="15" rows="2" name="newPostText"/>
+            </div>
+            <div>
+                <button>Add post</button>
+            </div>
+        </form>
+    )
+}
+
+const AddNewPostFormRedux = reduxForm({
+    form: "profileAddNewPostForm"
+})(AddNewPostForm)
 
 export default MyPosts;
