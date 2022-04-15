c7008a5f757dcc5612372f849be3276f7ec61255
Marina <dubinina.m.21@gmail.com>
2022-04-16 00:41:49 +0300

    Added method componentDidUpdate

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 2
+            userId = 23095
         }
 
         this.props.getUserProfile(userId);
