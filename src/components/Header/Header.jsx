9a09cf901656b0b957ef44898d828ea3fc135839
Marina <dubinina.m.21@gmail.com>
2022-04-17 18:16:36 +0300

    Realization login and logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = (props) => {
         <header className={s.header}>
             <img src="https://templates.iqonic.design/socialv/bs5/html/dist/assets/images/logo.png" alt=""/>
             <div className={s.loginBlock}>
-                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                {props.isAuth
+                    ? <div> {props.login} - <button onClick={props.logout}>Log out</button></div>
+                    : <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>
     )
